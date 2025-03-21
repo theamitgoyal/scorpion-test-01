b37f0a3143109b717b06d10fb50f10ad5b468848
Amit Goyal <31277286+theamitgoyal@users.noreply.github.com>
2025-03-21 12:41:49 +0100

    added play button prefab. updated contextual text to display only when cursor points at interactable object

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -4,23 +4,30 @@ public class PlayerInteract : MonoBehaviour
 {
     private Transform cameraTransform;
     [SerializeField] private float detectionDistance;
-    [SerializeField] private LayerMask mask;  
+    [SerializeField] private LayerMask mask;
+
+    PlayerUI playerUI;
     
     void Start()
     {
         cameraTransform = Camera.main.transform;
+        playerUI = GetComponent<PlayerUI>();
     }
 
     void Update()
     {
+        playerUI.UpdateText(string.Empty);
+        
         Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
         Debug.DrawRay(cameraTransform.position, cameraTransform.forward * detectionDistance, Color.red);
         RaycastHit hit;
         if (!Physics.Raycast(ray, out hit, detectionDistance, mask)) return;
+       
 
         if (hit.collider.GetComponent<Interactable>() != null)
         {
-            Debug.Log(hit.collider.GetComponent<Interactable>().promptMessage);
+            playerUI.UpdateText(hit.collider.GetComponent<Interactable>().promptMessage);
         }
+       
     }
 }
