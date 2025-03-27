1cb85fa5dc0fbb9971ca51bc745ca6c8ca8f697b
Amit Goyal <theamitgoyal@gmail.com>
2025-03-27 22:25:52 +0100

    updated dj console's nested prefabs and added scripts

--- Assets/Scripts/Music/MusicManager.cs
+++ Assets/Scripts/Music/MusicManager.cs
@@ -2,15 +2,5 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private MusicPlayer musicPlayer1, musicPlayer2;
 }
